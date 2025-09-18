11990ddb4e350d875fc57ef5cc785729940fe9c3
Daiwik Roy <uma.universetheory@gmail.com>
2025-09-18 17:02:37 +0530

    All stuttering issues fixed

--- engine.cpp
+++ engine.cpp
@@ -86,7 +86,6 @@ void render2dSprite(const char* filepath, SDL_FRect screen_render_part, float an
 }
 
 
-
 bool isRunning = true;
 bool initEngine(const char* TITLE, int SCREEN_WIDTH, int SCREEN_HEIGHT, bool USE_HARDWARE_ACCELERATION) {
     //Initialize SDL
